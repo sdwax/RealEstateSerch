42b409835f38cba18d13eae5e0dd9b8a53a8c700
Vasyl Kachan <v.kachan@spd-ukraine.com>
2021-08-15 17:51:04 +0300

    Initial commit

---
+++ src/main/java/com/project/realestateserch/RealEstateSerchApplication.java
@@ -0,0 +1,13 @@
+package com.project.realestateserch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateSerchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateSerchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/realestateserch/RealEstateSerchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.realestateserch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateSerchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
